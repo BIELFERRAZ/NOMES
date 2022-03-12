13dc65ba18bcc025fcbc39401f600a45f18a54b9
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-11 22:29:26 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿ string nome , sobrenome, sobrenomeEmMaiusculo;
+
+ Console.Write("Digita seu primeiro nome: ");
+ nome = Console.ReadLine()!;
+
+ Console.Write("Digite seu segundo nome: ");
+ sobrenome = Console.ReadLine()!;
+
+ sobrenomeEmMaiusculo = sobrenome.ToUpper();
+
+ Console.WriteLine($"Nome completo: {nome}  {sobrenome}" );
+
+ Console.WriteLine($"nome do catálogo: {sobrenomeEmMaiusculo}, {nome}");
